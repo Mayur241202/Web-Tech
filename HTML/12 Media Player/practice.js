abcda2a5694d37488375389d49b11d3bc5c8ea59
Mayur241202 <137171273+Mayur241202@users.noreply.github.com>
2024-09-08 18:45:03 +0530

    Add files via upload

---
+++ HTML/12 Media Player/practice.js
@@ -0,0 +1,17 @@
+console.log("Hello World")
+alert("Hello World")
+document.write("Unother Hello World")
+document.write("<h1>Unother Hello World </h1>")
+
+// Add a two number
+
+document.write("program 1 : Sum of two")
+// static
+// let a=5;
+// let b = 6;
+
+let a = prompt("Enter a First Number");
+let b = prompt("Enter a Second Number");
+let c = parseInt(a)+parseInt(b)
+// let c = a + b;
+console.log("The sum of "+a+"and"+b+"is"+c)
